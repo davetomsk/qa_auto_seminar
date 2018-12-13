14a27213b1dafb04577fd7c2745507c4cd471c2e
David.andrievskiy <David.Andrievskiy@sidenis.com>
2018-12-13 15:00:00 +0700

    first

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main (String args[]) {
+
+        int a = 5;
+        a++;
+        a++;
+        a++;
+
+//        System.out.println("Hello " + args[0]);
+//
+//        System.out.println("Hello " + args[1]);
+//
+//        System.out.println("Hello " + args[2]);
+
+    }
+
+}
