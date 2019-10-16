b50cef5aa87dda8e6cd75ce67794613f4f34386c
davetomsk <41429679+davetomsk@users.noreply.github.com>
2019-10-16 21:29:46 +0700

    Mox

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 //
 //        System.out.println("Hello " + args[1]);
 
-       System.out.println("Hello " + args[2]);
+       System.out.println("Hello Moxnata" + args[2]);
 
     }
 
